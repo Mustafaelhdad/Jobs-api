ef614fa3a3346a174d23b73ca0726a0197181be5
Mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2023-03-19 23:42:39 +0200

    ..

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,26 +3,32 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
 const jwt = require("jsonwebtoken");
 
+// const register = async (req, res) => {
+//   // const { name, email, password } = req.body;
+//   // if (!name || !email || !password) {
+//   //   throw new BadRequestError("Please provide name, email, and password!");
+//   // }
+//   // const { name, email, password } = req.body;
+
+//   // const salt = await bcrypt.genSalt(10);
+//   // const hashedPassword = await bcrypt.hash(password, salt);
+
+//   // const tempUser = { name, email, password: hashedPassword };
+
+//   const user = await User.create({ ...req.body });
+//   // const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
+//   //   expiresIn: "30d",
+//   // });
+
+//   res
+//     .status(StatusCodes.CREATED)
+//     .json({ user: { name: user.getName() }, token });
+// };
+
 const register = async (req, res) => {
-  // const { name, email, password } = req.body;
-  // if (!name || !email || !password) {
-  //   throw new BadRequestError("Please provide name, email, and password!");
-  // }
-  // const { name, email, password } = req.body;
-
-  // const salt = await bcrypt.genSalt(10);
-  // const hashedPassword = await bcrypt.hash(password, salt);
-
-  // const tempUser = { name, email, password: hashedPassword };
-
   const user = await User.create({ ...req.body });
-  const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
-    expiresIn: "30d",
-  });
-
-  res
-    .status(StatusCodes.CREATED)
-    .json({ user: { name: user.getName() }, token });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
